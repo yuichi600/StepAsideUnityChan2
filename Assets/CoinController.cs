74026dabc056b100779f97f8209b35f5dbb98071
Murata-Optiplex7040 <y.murata@logic-design.jp>
2019-06-09 18:30:30 +0900

    [ Create ] Lesson6やり直し

---
+++ Assets/CoinController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+		
+		public GameObject unityPlace;
+		public float distance;
+        // Use this for initialization
+        void Start () {
+				
+				this.unityPlace = GameObject.Find("unitychan");
+
+                //回転を開始する角度を設定
+                this.transform.Rotate (0, Random.Range (0, 360), 0);
+        }
+        
+        // Update is called once per frame
+        void Update () {
+                //回転
+                this.transform.Rotate (0, 3, 0);
+				//Destroy(this.gameObject);
+				distance = unityPlace.transform.position.z-this.transform.position.z;
+				if(distance>0){
+					Destroy(this.gameObject);
+				}				
+        }
+}
---
+++ Assets/TrafficConeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TrafficConeController : MonoBehaviour {
+		
+		public GameObject unityPlace;
+		public float distance;
+        // Use this for initialization
+        void Start () {
+				
+				this.unityPlace = GameObject.Find("unitychan");
+        }
+        
+        // Update is called once per frame
+        void Update () {
+				//Destroy(this.gameObject);
+				distance = unityPlace.transform.position.z-this.transform.position.z;
+				if(distance>0){
+					Destroy(this.gameObject);
+				}
+        }
+}
